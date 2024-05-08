0a6859470c785969e1e147cd532ee1b416818bc2
AMAN SHARMA <ishusharma1512003@gmail.co>
2024-05-08 16:47:45 +0530

    firstqustionarray

---
+++ learn/first.cpp
@@ -0,0 +1,84 @@
+//  #include<iostream>
+// using namespace std;
+// int main(){
+// // int a = 50;
+// // int b = 50;
+
+ 
+// // if( a,b == 50){
+// //     cout<<"match";
+// // }else{
+// //     cout<<"not match";
+// // }
+
+ 
+
+// // int arr [5]={1,2,3,4,};
+// // for(int i =0; i<arr[i]; i++){
+// //     cout<<arr[i];
+  
+// // }
+
+// // int arr [5]={1,2,3,4,};
+// // for(int i =3; i<arr[i]; i--){
+// //     cout<<arr[i];
+  
+// // }
+
+
+// // int arr [5]={1,2,3,4,};
+
+// //    if(arr[0]==arr[1]){
+// //     cout<<"match";
+// //    }else{
+// //     cout<<" not match";
+// //    }
+   
+
+// //     int arr[9]={1,2,3,4,5,6,7,8,9};
+// //     for (int i=8; i>=0; i--){
+// //     cout<<arr[i];
+
+// //    }
+  
+// //   int arr [10]={1,2,3,4,5,6,7,8,9};
+// //   for(int i=0; i<9; i++ ){
+// //     cout<<arr[i];
+// //   }
+
+
+// //   int arr[5]={"a","b","c","d"};
+// //   if(arr[0]==arr[5]){
+// //     cout<<"match";
+// //   }else{
+// //     cout<<"not match";
+// //   }
+
+// char add[4]={"a","b","c","d"};
+//   if(add[1]==add[2]){
+//     cout<<"match";
+//   }else{
+//     cout<<"not match";
+//   }
+
+
+// }
+
+
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char add[4] = {'a', 'b', 'c', 'd'};
+    if (add[1] == 'b' && add[2] == 'c') {
+        cout << "match";
+    } else {
+        cout << "not match";
+    }
+    return 0;
+}
